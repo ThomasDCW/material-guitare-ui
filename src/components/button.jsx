765d3a0f2c955ba90e05de84457c7611e2517b52
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-22 19:55:01 +0200

    add some buttons

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,5 +1,38 @@
-import { Button } from '@mui/material';
+import { Button, IconButton, Stack } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 export default function MyButton() {
-  return <Button variant='contained'>HELLO</Button>;
+  return (
+    <>
+      <Stack direction='column' alignItems='center' spacing={2}>
+        <Button variant='contained' size='small'>
+          HELLO
+        </Button>
+        <Button variant='contained' color='success' size='medium'>
+          SUCCES
+        </Button>
+        <Button variant='outlined' color='error' size='large'>
+          Error
+        </Button>
+        <IconButton
+          color='primary'
+          aria-label='upload picture'
+          component='span'
+        >
+          <PhotoCamera />
+        </IconButton>
+        <Button variant='outlined' startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        <Button variant='contained' endIcon={<SendIcon />}>
+          Send
+        </Button>
+        <IconButton aria-label='delete' size='large'>
+          <DeleteIcon fontSize='inherit' />
+        </IconButton>
+      </Stack>
+    </>
+  );
 }
